76f1c9dbdf6ec26e6a7aba8974a32e07475b93c9
Daniel Magalhaes Correa <wdanieldmc@gmail.com>
2021-05-08 23:40:34 -0300

    Tarefa 2 - Ajustes aula 01

--- app/src/main/java/com/desenvmovel/dp_aula01/MainActivity.java
+++ app/src/main/java/com/desenvmovel/dp_aula01/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Criação das variáveis que irão fazer conexão com o layout activity_main.xml
     private EditText mEditText;
     private TextView mTextView;
 
@@ -17,11 +18,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Link para os componentes do layout (instanciando)
         mEditText = findViewById(R.id.idEditText);
         mTextView = findViewById(R.id.idTextView);
 
     }
 
+    //Método que será chamado ao clicar no botão confirmar
     public void Confirmar(View view) {
         String textoDigitado = mEditText.getText().toString();
         mTextView.setText("O curso digitado é " + textoDigitado);
